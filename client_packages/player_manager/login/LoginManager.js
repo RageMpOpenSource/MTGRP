697109f9036a7c0d95e555bb4d3501bbeb4ee1fe
Ahmed <ahmad.alwasifi@gmail.com>
2020-05-30 14:58:01 +0200

    LoginManager.js converted.

--- client_packages/player_manager/login/LoginManager.js
+++ client_packages/player_manager/login/LoginManager.js
@@ -1,60 +1,56 @@
-﻿var login_view = API.createCamera(new Vector3(682.7154, 1063.229, 353.9427), new Vector3(-3.673188, -10.46521, -18.79283));
-API.pointCameraAtPosition(login_view, new Vector3(718.9848, 1194.599, 325.2131));
+﻿let login_view = mp.cameras.new('default', new mp.Vector3(682.7154, 1063.229, 353.9427), new mp.Vector3(-3.673188, -10.46521, -18.79283), 40);
+login_view.pointAtCoord(718.9848, 1194.599, 325.2131);
 
 var login_browser = null;
 
 Event.OnResourceStart.connect(function () {
-
-    var res = API.getScreenResolutionMaintainRatio();
-	var pos = resource.JsFunctions.scaleCoordsToReal({ X: res.Width, Y:  res.Height});
-    login_browser = API.createCefBrowser(pos.X, pos.Y);
-    API.waitUntilCefBrowserInit(login_browser);
-    API.setCefBrowserPosition(login_browser, 0, 0);
-    API.loadPageCefBrowser(login_browser, "player_manager/login/Login.html");
-    API.showCursor(true);
-    API.setCanOpenChat(false);
-    //API.setCefDrawState(true);
-   
+    login_browser = mp.browsers.new("package://player_manager/login/Login.html");
+    mp.gui.cursor.show(true, true);
+    mp.gui.chat.activate(false);
 });
 
-Event.OnServerEventTrigger.connect(function (eventName, args) {
+mp.events.add('onPlayerConnectedEx', (isReg) => {
+    login_view.setActive(true);
+    mp.game.cam.renderScriptCams(true, false, 2000, true, false);
+    login_browser.execute(`update_box_info('${mp.players.local.name}', ${isReg});`);
+});
 
-    switch (eventName) {
-        case "onPlayerConnectedEx":
-            API.setActiveCamera(login_view);
+mp.events.add('login_finished', () => {
+    login_view.setActive(false);
+    mp.game.cam.renderScriptCams(false, false, 2000, true, false);
+    mp.gui.chat.activate(true);
+});
 
-            API.sleep(500);
-            login_browser.call("update_box_info", API.getPlayerName(API.getLocalPlayer()), args[0]);
-            break;
-        case "login_finished":
-            API.setActiveCamera(null);
-            API.setCanOpenChat(true);
-            break;
-        case "login_error":
-            login_browser.call("login_error", args[0]);
-            break;
-        case "hide_login_browser":
-            if (login_browser != null) {
-                API.showCursor(false);
-                API.destroyCefBrowser(login_browser);
-                //API.setCefDrawState(false);
-                login_browser = null;
-            }
-            break;
-        case "admin_pin_check":
-            var adminPin = API.getUserInput("", 5);
-            API.triggerServerEvent("admin_pin_check", adminPin);
-            break;
-        case "create_admin_pin":
-            var adminPinCreate = API.getUserInput("", 5);
-            API.triggerServerEvent("create_admin_pin", adminPinCreate);
-            break;
+mp.events.add('login_error', (err) => {
+    login_browser.execute(`login_error('${err}');`);
+});
+
+mp.events.add('hide_login_browser', () => {
+    if (login_browser != null) {
+        mp.gui.cursor.show(false, false);
+        login_browser.destroy();
+        mp.gui.chat.activate(true);
+        login_browser = null;
     }
 });
 
-function attempt_login(password) {
-    API.triggerServerEvent("attempt_login", password);
-}
+mp.events.add('admin_pin_check', () => {
+    var adminPin = API.getUserInput("", 5); // TODO: Use alternative
+    mp.events.callRemote("admin_pin_check", adminPin);
+});
+mp.events.add('create_admin_pin', () => {
+    var adminPinCreate = API.getUserInput("", 5); // TODO: Use alternative
+    mp.events.callRemote("create_admin_pin", adminPinCreate);
+});
+
+
+//CEF event
+mp.events.add('attempt_login', (password) => {
+    mp.events.callRemote("attempt_login", password);
+});
+
+/*
+MANUAL LOGIN DISABLED
 
 Event.OnKeyDown.connect((sender, e) =>
 {
@@ -65,3 +61,4 @@ Event.OnKeyDown.connect((sender, e) =>
 		login_browser = null;
 	}
 });
+*/
